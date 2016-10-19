ae61c4b7ebd31ccf5abe9313421e6525ae4f0aa8
Denis Cheung <d.s.h.cheung@gmail.com>
2016-10-19 16:36:12 +0800

    recipe form partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :courses, only: [:index] do
-    resources :recipes, except: [:new, :edit], controller: 'course_recipes'
+    resources :recipes, only: [:index, :show], controller: 'course_recipes'
   end
 
   resources :recipes
